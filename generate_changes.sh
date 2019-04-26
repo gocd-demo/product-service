20dfa60d16b205a73dc8647b92cf590da0cfc7b2
Sheroy Marker <smarker@thoughtworks.com>
2019-04-26 14:54:55 -0700

    Spiritual leadership should remain spiritual leadership and the temporal power should not become too important in any church. - Eleanor Roosevelt

---
+++ generate_changes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NUMBER_OF_COMMITS=20
+SLEEP_TIME=300
+
+for i in `seq 1 $NUMBER_OF_COMMITS`;
+do
+	echo "This is a new line $i" >> demo_trigger_file.txt
+	git commit -am "`fortune`"
+        git push
+	sleep $SLEEP_TIME
+done
